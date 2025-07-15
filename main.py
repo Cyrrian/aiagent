7fe02754b12bf6ace3f3b06b759415c7b2a8bfd0
Cyrrian <william.r.stewart@gmail.com>
2025-07-15 17:08:07 -0500

    Added --verbose option

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -9,17 +10,32 @@ def main():
     client = genai.Client(api_key=api_key)
     model = 'gemini-2.0-flash-001'
 
-    if len(sys.argv[1]) == 0:
+    verbose = False
+
+    args = sys.argv[1:]
+
+    if not args:
         print("no prompt provided")
         exit(1)
 
-    query = sys.argv[1]
+    if args[-1] == '--verbose':
+        verbose = True
+        args = args[:-1]
+
+    query = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=query)]),
+    ]
     response = client.models.generate_content(model=model, contents=query)
 
     print(response.text)
-    print('**** TOKENS USED ****')
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if verbose:
+        print(f"User prompt: {query}")
+        print('**** TOKENS USED ****')
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
