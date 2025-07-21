569fb84e327ab69116f1071eb496337bd0202c21
Cyrrian <william.r.stewart@gmail.com>
2025-07-21 14:22:18 -0500

    Refactor

--- main.py
+++ main.py
@@ -6,41 +6,51 @@ from google.genai import types
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-    model = 'gemini-2.0-flash-001'
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
-    verbose = False
+    args = []
+    verbose = '--verbose' in sys.argv
 
-    args = sys.argv[1:]
+    for arg in sys.argv[1:]:
+        if not arg.startswith('--'):
+            args.append(arg)
 
     if not args:
-        print("no prompt provided")
+        print('AI Agent')
+        print('\nUsage: python main.py "PROMPT" [--verbose]')
         exit(1)
 
-    if args[-1] == '--verbose':
-        verbose = True
-        args = args[:-1]
+    API_KEY = os.environ.get("GEMINI_API_KEY")
+    AI_CLIENT = genai.Client(api_key=API_KEY)
+    AI_MODEL = 'gemini-2.0-flash-001'
+    SYSTEM_PROMPT = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
-    query = " ".join(args)
+    user_prompt = " ".join(args)
 
     messages = [
-        types.Content(role="user", parts=[types.Part(text=query)]),
+        types.Content(role='user', parts=[types.Part(text=user_prompt)]),
     ]
+
+    if verbose:
+        print('\nUSER PROMPT:')
+        print(f'\t{user_prompt}')
+
+    execute_prompt(AI_CLIENT, AI_MODEL, SYSTEM_PROMPT, messages, verbose)
+        
+
+def execute_prompt(client, model, system_prompt, messages, verbose):
     response = client.models.generate_content(
         model=model, 
         contents=messages, 
         config=types.GenerateContentConfig(system_instruction=system_prompt)
         )
-
-    print(response.text)
+    
+    print('\nRESPONSE:')
+    print(f'\t{response.text}')
 
     if verbose:
-        print(f"User prompt: {query}")
-        print('**** TOKENS USED ****')
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print('TOKENS USED:')
+        print(f'\tPrompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'\tResponse tokens: {response.usage_metadata.candidates_token_count}')
 
 if __name__ == "__main__":
     main()
