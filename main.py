c6823e921fe3821d608cb88cb3c4edf8c7f3308c
Cyrrian <william.r.stewart@gmail.com>
2025-07-22 13:43:48 -0500

    added first function declaration

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.get_files_info import schema_get_files_info
+
 def main():
     load_dotenv()
 
@@ -22,7 +24,23 @@ def main():
     API_KEY = os.environ.get("GEMINI_API_KEY")
     AI_CLIENT = genai.Client(api_key=API_KEY)
     AI_MODEL = 'gemini-2.0-flash-001'
-    SYSTEM_PROMPT = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    SYSTEM_PROMPT = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
+    config = types.GenerateContentConfig(tools=[available_functions], system_instruction=SYSTEM_PROMPT)
 
     user_prompt = " ".join(args)
 
@@ -34,18 +52,23 @@ def main():
         print('\nUSER PROMPT:')
         print(f'\t{user_prompt}')
 
-    execute_prompt(AI_CLIENT, AI_MODEL, SYSTEM_PROMPT, messages, verbose)
+    execute_prompt(AI_CLIENT, AI_MODEL, config, messages, verbose)
         
 
-def execute_prompt(client, model, system_prompt, messages, verbose):
+def execute_prompt(client, model, config, messages, verbose):
     response = client.models.generate_content(
         model=model, 
         contents=messages, 
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
-        )
+        config=config
+    )
     
     print('\nRESPONSE:')
     print(f'\t{response.text}')
+
+    if response.function_calls:
+        print('\nFUNCTION CALLS:')
+        for call in response.function_calls:
+            print(f'{call.name}({call.args})')
 
     if verbose:
         print('TOKENS USED:')
